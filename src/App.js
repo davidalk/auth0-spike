d954aad78cba9cf1534b6e3fad15d447d9bebff0
davidalk <david.alkanani@gmail.com>
2019-02-26 16:48:04 +0000

    provide a logout button

--- src/App.js
+++ src/App.js
@@ -8,32 +8,43 @@ import {
   renewSession
 } from './auth'
 
+
 const isCallback = () => {
   const hash = window.location.hash
   return (/access_token|id_token|error/.test(hash))
 }
 
-const Content = () => (
+const Content = ({handleLogout}) => (
   <div>
     <p>You are in the App!!!</p>
-    <button onClick={() => logout()}>Logout</button>
+    <button onClick={() => handleLogout()}>Logout</button>
   </div>
 )
+
+const LoggedOut = () => (<div>You are not logged in!!!!</div>)
 
 class App extends Component {
   constructor(props) {
     super(props)
+
+    this.handleLogout = this.handleLogout.bind(this)
 
     this.state = {
       authenticated: false
     }
   }
 
+  handleLogout() {
+    logout()
+    this.setState({authenticated: false})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.authenticated && <Content/>}
+          {this.state.authenticated ? 
+          <Content handleLogout={this.handleLogout}/> : <LoggedOut/>}
         </header>
       </div>
     )
